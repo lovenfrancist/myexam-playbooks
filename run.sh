18064bc0baeba4ec0587e08958acc1d5df0b6a6f
loven <loven.francis+isentia@gmail.com>
2018-11-02 14:40:13 +0800

    added bash script default parameters

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-sudo ansible-playbook ~/myexam-playbooks/myexam.yaml -i ~/myexam-playbooks/inventory --extra-vars "proj_env=dev" &&
-(cd ~/myexam && exec ~/gems/bin/bundle exec ~/gems/bin/jekyll build)
+ARG1=${1:-dev} # 'dev' (default) OR 'staging'
+ARG2=${2:-~/myexam} # default jekyll project root dir
+ARG3=${3:-jekyll_project_path="$ARG2"} # extra vars for ansible playbook param
+
+# echo "$ARG1"
+# echo "$ARG2"
+# echo "$ARG3"
+
+# run ansible playbook
+sudo ansible-playbook myexam.yaml -i inventory --extra-vars "proj_env='$ARG1'"
+
+# build jekyll site
+(cd "$ARG2" && exec ~/gems/bin/bundle exec ~/gems/bin/jekyll build)
